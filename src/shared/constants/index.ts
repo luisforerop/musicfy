7680464606c8b5e5b3ee6e223015a780d8cc85fa
Luis <Luis@LAPTOP-0EFHT9GS>
2022-04-23 16:56:32 -0500

    Create constants

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,4 @@
+export const BASE_URL_API = 'https://i8rmpiaad2.execute-api.us-east-1.amazonaws.com/dev/api'
+export const ARTISTS_URL = `${BASE_URL_API}/artists`
+export const ALBUMS_URL = `${BASE_URL_API}/artists/1/albums`
+export const SONGS_URL = `${BASE_URL_API}/albums/1/songs`
